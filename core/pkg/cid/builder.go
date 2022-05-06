4e29a9e141b797ade198f24384e6735253c111f6
Siarhei Valasovich <75541419+svalasovich@users.noreply.github.com>
2022-05-06 15:36:48 +0300

    Cid Hash compatibility (#3)
    * add compatibility for cid hash algorithm
    
    * clean up
    
    * updated ddc_bucket contract

--- core/pkg/cid/builder.go
+++ core/pkg/cid/builder.go
@@ -2,25 +2,22 @@ package cid
 
 import (
 	"github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multihash"
 )
 
 const (
-	defaultMhType   = multihash.BLAKE2B_MAX - 32
-	defaultMhLength = 32
-	defaultCodec    = cid.Raw
+	Blake2b256   = 0xb220
+	defaultCodec = cid.Raw
 )
 
 type Builder struct {
 	cidBuilder cid.V1Builder
 }
 
-func CreateBuilder(codec uint64, mhType uint64) *Builder {
-	return &Builder{cidBuilder: cid.V1Builder{Codec: codec, MhType: mhType}}
-}
-
-func DefaultBuilder() *Builder {
-	return &Builder{cidBuilder: cid.V1Builder{Codec: defaultCodec, MhType: defaultMhType, MhLength: defaultMhLength}}
+func CreateBuilder(mhType uint64) *Builder {
+	if mhType == 0 {
+		mhType = Blake2b256
+	}
+	return &Builder{cidBuilder: cid.V1Builder{Codec: defaultCodec, MhType: mhType}}
 }
 
 func (b *Builder) Build(data []byte) (string, error) {
--- core/pkg/cid/builder_test.go
+++ core/pkg/cid/builder_test.go
@@ -1,28 +1,13 @@
 package cid
 
 import (
-	"github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestGetPieceCid_Sha2_256(t *testing.T) {
 	//given
-	testSubject := CreateBuilder(cid.Raw, multihash.SHA2_256)
-	expectedCid := "bafkreigaknpexyvxt76zgkitavbwx6ejgfheup5oybpm77f3pxzrvwpfdi"
-
-	//when
-	c, err := testSubject.Build([]byte("Hello world!"))
-
-	//then
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCid, c)
-}
-
-func TestGetPieceCid_Blake2b_256(t *testing.T) {
-	//given
-	testSubject := DefaultBuilder()
+	testSubject := CreateBuilder(Blake2b256)
 	expectedCid := "bafk2bzacea73ycjnxe2qov7cvnhx52lzfp6nf5jcblnfus6gqreh6ygganbws"
 
 	//when
