58e5eb6c3558ce1281bf12f7b03a786975a8eb74
svalasovich <svalasovich@gmail.com>
2022-08-11 20:30:29 +0400

    updated LogRecord structure

--- model/domain/logrecord.go
+++ model/domain/logrecord.go
@@ -7,11 +7,34 @@ import (
 	"time"
 )
 
-type LogRecord struct {
-	Ack       []byte
-	Timestamp *time.Time
-	Address   string
-}
+type (
+	LogRecord struct {
+		Request   IsRequest
+		Timestamp *time.Time
+		Address   string
+		Resources uint32
+	}
+
+	WriteRequest struct {
+		Cid       string
+		BucketId  uint32
+		Size      uint32
+		Signature *Signature
+	}
+
+	ReadRequest struct {
+		Cid      string
+		BucketId uint32
+	}
+
+	QueryRequest struct {
+		Query *Query
+	}
+
+	IsRequest interface {
+		isRequest()
+	}
+)
 
 var _ Protobufable = (*LogRecord)(nil)
 
@@ -19,8 +42,10 @@ func (l *LogRecord) ToProto() *pb.LogRecord {
 	result := &pb.LogRecord{
 		Timestamp: timestamppb.New(*l.Timestamp),
 		Address:   l.Address,
-		Ack:       l.Ack,
+		Resources: l.Resources,
 	}
+
+	l.requestToProto(result)
 
 	return result
 }
@@ -28,8 +53,10 @@ func (l *LogRecord) ToProto() *pb.LogRecord {
 func (l *LogRecord) ToDomain(pbLogRecord *pb.LogRecord) {
 	timestamp := pbLogRecord.Timestamp.AsTime()
 	l.Timestamp = &timestamp
+
 	l.Address = pbLogRecord.Address
-	l.Ack = pbLogRecord.Ack
+	l.Resources = pbLogRecord.Resources
+	l.Request = requestToDomain(pbLogRecord)
 }
 
 func (l *LogRecord) MarshalProto() ([]byte, error) {
@@ -45,3 +72,56 @@ func (l *LogRecord) UnmarshalProto(logRecordAsBytes []byte) error {
 	l.ToDomain(pbLogRecord)
 	return nil
 }
+
+func (l *LogRecord) requestToProto(pbLogRecord *pb.LogRecord) {
+	switch record := l.Request.(type) {
+	case *WriteRequest:
+		signature := record.Signature.ToProto()
+		pbLogRecord.Request = &pb.LogRecord_WriteRequest{WriteRequest: &pb.WriteRequest{
+			BucketId:  record.BucketId,
+			Size:      record.Size,
+			Cid:       record.Cid,
+			Signature: signature,
+		}}
+	case *ReadRequest:
+		pbLogRecord.Request = &pb.LogRecord_ReadRequest{ReadRequest: &pb.ReadRequest{
+			Cid:      record.Cid,
+			BucketId: record.BucketId,
+		}}
+	case *QueryRequest:
+		pbLogRecord.Request = &pb.LogRecord_QueryRequest{QueryRequest: &pb.QueryRequest{
+			Query: record.Query.ToProto(),
+		}}
+	}
+}
+
+func requestToDomain(pbLogRecord *pb.LogRecord) IsRequest {
+	switch record := pbLogRecord.Request.(type) {
+	case *pb.LogRecord_WriteRequest:
+		writeRecord := record.WriteRequest
+
+		signature := &Signature{}
+		signature.ToDomain(writeRecord.Signature)
+
+		return &WriteRequest{
+			BucketId:  writeRecord.BucketId,
+			Size:      writeRecord.Size,
+			Cid:       writeRecord.Cid,
+			Signature: signature,
+		}
+	case *pb.LogRecord_ReadRequest:
+		readRecord := record.ReadRequest
+		return &ReadRequest{Cid: readRecord.Cid, BucketId: readRecord.BucketId}
+	case *pb.LogRecord_QueryRequest:
+		queryRecord := record.QueryRequest
+		query := &Query{}
+		query.ToDomain(queryRecord.Query)
+		return &QueryRequest{Query: query}
+	}
+
+	return nil
+}
+
+func (w *WriteRequest) isRequest() {}
+func (q *QueryRequest) isRequest() {}
+func (r *ReadRequest) isRequest()  {}
