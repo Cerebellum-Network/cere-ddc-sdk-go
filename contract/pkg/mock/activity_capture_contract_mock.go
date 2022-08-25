0a9ca6a292685ddc5f8657d20b923eb86091358b
svalasovich <svalasovich@gmail.com>
2022-08-25 17:28:08 +0400

    updated mock activity capture client

--- contract/pkg/mock/activity_capture_contract_mock.go
+++ contract/pkg/mock/activity_capture_contract_mock.go
@@ -3,13 +3,15 @@ package mock
 import (
 	"context"
 	"encoding/hex"
+	"github.com/centrifuge/go-substrate-rpc-client/v2/types"
 	"github.com/cerebellum-network/cere-ddc-sdk-go/contract/pkg/actcapture"
+	"math/big"
 	"math/rand"
 )
 
 type (
 	activityCaptureContractMock struct {
-		commit string
+		commit actcapture.Commit
 	}
 )
 
@@ -19,12 +21,13 @@ func CreateActivityCaptureContractMock() actcapture.ActivityCaptureContract {
 	return &activityCaptureContractMock{}
 }
 
-func (a *activityCaptureContractMock) GetCommit() (string, error) {
-	return a.commit, nil
+func (a *activityCaptureContractMock) GetCommit() (*actcapture.Commit, error) {
+	commit := a.commit
+	return &commit, nil
 }
 
-func (a *activityCaptureContractMock) SetCommit(ctx context.Context, data string) (string, error) {
-	a.commit = data
+func (a *activityCaptureContractMock) SetCommit(ctx context.Context, hash []byte, resources uint64) (string, error) {
+	a.commit = actcapture.Commit{Hash: types.NewHash(hash), Resources: types.NewU128(*new(big.Int).SetUint64(resources))}
 
 	token := make([]byte, 32)
 	rand.Read(token)
