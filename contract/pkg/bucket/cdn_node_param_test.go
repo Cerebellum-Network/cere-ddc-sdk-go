28b1005d738c1a30c986c17f93203f4d9d1730a2
Valeriy Gello <valeriy.gello@pluto.tv>
2023-03-14 22:04:21 +0200

    Add structure for CDN node params json marshaling/reading

---
+++ contract/pkg/bucket/cdn_node_param_test.go
@@ -0,0 +1,48 @@
+package bucket
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReadCDNNodeParams(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantP   CDNNodeParams
+		wantErr bool
+	}{
+		{
+			name: "Positive test",
+			args: args{s: `{"url":"http://localhost:8080", "publicKey": "NodePublicKey", "size":1, "location":"US"}`},
+			wantP: CDNNodeParams{
+				Url:       "http://localhost:8080",
+				Size:      1,
+				Location:  "US",
+				PublicKey: "NodePublicKey",
+			},
+			wantErr: false,
+		},
+		{
+			name:    "Wrong structure",
+			args:    args{s: `{"}`},
+			wantP:   CDNNodeParams{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotP, err := ReadCDNNodeParams(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReadCDNNodeParams() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotP, tt.wantP) {
+				t.Errorf("ReadCDNNodeParams() gotP = %v, want %v", gotP, tt.wantP)
+			}
+		})
+	}
+}
