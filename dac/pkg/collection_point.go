5a6da118ea4f719b1782d5acc6006f09d9a16a7c
Ivan Dmitriev <100341+idmitriev@users.noreply.github.com>
2023-10-04 13:55:47 +0400

    Fix sessionId type for Fulfillment message in dac module

--- dac/pkg/collection_point.go
+++ dac/pkg/collection_point.go
@@ -81,7 +81,7 @@ func (d dacCollectionPoint) SaveFulfillment(fulfillment Fulfillment) error {
 }
 
 func SignFulfillment(fulfillment *Fulfillment, scheme crypto.Scheme) error {
-	signature, err := scheme.Sign([]byte(fulfillment.Cid + base64.StdEncoding.EncodeToString(fulfillment.SessionId) + fulfillment.RequestId + strconv.FormatUint(fulfillment.FulfilledTimestamp, 10)))
+	signature, err := scheme.Sign([]byte(fulfillment.Cid + base64.StdEncoding.EncodeToString([]byte(fulfillment.SessionId)) + fulfillment.RequestId + strconv.FormatUint(fulfillment.FulfilledTimestamp, 10)))
 	if err != nil {
 		return err
 	}
