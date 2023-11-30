2687df0fd27a11f938e47e9c5e4f5cd1f64022ce
Alisher A. Khassanov <a.khssnv@gmail.com>
2023-11-30 14:50:47 +0600

    Add `pallet-ddc-nodes` API wrapper

---
+++ blockchain/pallets/ddcnodes.go
@@ -0,0 +1,101 @@
+package pallets
+
+import (
+	gsrpc "github.com/centrifuge/go-substrate-rpc-client/v4"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types/codec"
+)
+
+const (
+	MaxCdnNodeParamsLen     = 2048
+	MaxStorageNodeParamsLen = 2048
+	MaxHostLen              = 255
+)
+
+type CdnNode struct {
+	PubKey     CdnNodePubKey
+	ProviderId types.AccountID
+	ClusterId  types.Option[ClusterId]
+	Props      CdnNodeProps
+}
+
+type CdnNodeProps struct {
+	Host     [MaxHostLen]types.U8
+	HttpPort types.U16
+	GrpcPort types.U16
+	P2pPort  types.U16
+}
+
+type StorageNode struct {
+	PubKey     StorageNodePubKey
+	ProviderId types.AccountID
+	ClusterId  types.Option[ClusterId]
+	Props      StorageNodeProps
+}
+
+type StorageNodeProps struct {
+	Host     [MaxHostLen]types.U8
+	HttpPort types.U16
+	GrpcPort types.U16
+	P2pPort  types.U16
+}
+
+type DdcNodesApi struct {
+	substrateApi *gsrpc.SubstrateAPI
+	meta         *types.Metadata
+}
+
+func NewDdcNodesApi(substrateAPI *gsrpc.SubstrateAPI, meta *types.Metadata) *DdcNodesApi {
+	return &DdcNodesApi{
+		substrateAPI,
+		meta,
+	}
+}
+
+func (api *DdcNodesApi) GetStorageNodes(pubkey StorageNodePubKey) (types.Option[StorageNode], error) {
+	maybeNode := types.NewEmptyOption[StorageNode]()
+
+	bytes, err := codec.Encode(pubkey)
+	if err != nil {
+		return maybeNode, err
+	}
+
+	key, err := types.CreateStorageKey(api.meta, "DdcNodes", "StorageNodes", bytes)
+	if err != nil {
+		return maybeNode, err
+	}
+
+	var node StorageNode
+	ok, err := api.substrateApi.RPC.State.GetStorageLatest(key, &node)
+	if !ok || err != nil {
+		return maybeNode, err
+	}
+
+	maybeNode.SetSome(node)
+
+	return maybeNode, nil
+}
+
+func (api *DdcNodesApi) GetCdnNodes(pubkey CdnNodePubKey) (types.Option[CdnNode], error) {
+	maybeNode := types.NewEmptyOption[CdnNode]()
+
+	bytes, err := codec.Encode(pubkey)
+	if err != nil {
+		return maybeNode, err
+	}
+
+	key, err := types.CreateStorageKey(api.meta, "DdcNodes", "CdnNodes", bytes)
+	if err != nil {
+		return maybeNode, err
+	}
+
+	var node CdnNode
+	ok, err := api.substrateApi.RPC.State.GetStorageLatest(key, &node)
+	if !ok || err != nil {
+		return maybeNode, err
+	}
+
+	maybeNode.SetSome(node)
+
+	return maybeNode, nil
+}
