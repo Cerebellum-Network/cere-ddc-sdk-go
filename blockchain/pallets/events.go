d2fa6dcb75522f3aba0d9fd2b41f4a4e90d7fe5d
Alisher A. Khassanov <a.khssnv@gmail.com>
2024-01-02 17:57:33 +0600

    Create chain custom event records type

--- blockchain/pallets/events.go
+++ blockchain/pallets/events.go
@@ -4,7 +4,18 @@ import (
 	"sync"
 
 	"github.com/centrifuge/go-substrate-rpc-client/v4/registry/parser"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
 )
+
+type Events struct {
+	types.EventRecords
+
+	DdcClusters_ClusterCreated      []EventDdcClustersClusterCreated      //nolint:stylecheck,golint
+	DdcClusters_ClusterNodeAdded    []EventDdcClustersClusterNodeAdded    //nolint:stylecheck,golint
+	DdcClusters_ClusterNodeRemoved  []EventDdcClustersClusterNodeRemoved  //nolint:stylecheck,golint
+	DdcClusters_ClusterParamsSet    []EventDdcClustersClusterParamsSet    //nolint:stylecheck,golint
+	DdcClusters_ClusterGovParamsSet []EventDdcClustersClusterGovParamsSet //nolint:stylecheck,golint
+}
 
 type NewEventSubscription[T any] struct {
 	ch   chan T
