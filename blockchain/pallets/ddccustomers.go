0214d43aca335fb85cafdda4dbb344f9c5ddf796
Alisher A. Khassanov <a.khssnv@gmail.com>
2024-01-03 18:17:17 +0600

    Add `ddc-customers` events subscription

--- blockchain/pallets/ddccustomers.go
+++ blockchain/pallets/ddccustomers.go
@@ -1,6 +1,7 @@
 package pallets
 
 import (
+	"math"
 	"sync"
 
 	gsrpc "github.com/centrifuge/go-substrate-rpc-client/v4"
@@ -248,4 +249,190 @@ func (api *ddcCustomersApi) GetLedger(owner types.AccountID) (types.Option[Accou
 	maybeLedger.SetSome(accountsLedger)
 
 	return maybeLedger, nil
+}
+
+func (api *ddcCustomersApi) SubscribeNewDeposited() (*NewEventSubscription[EventDdcCustomersDeposited], error) {
+	api.mu.Lock()
+	defer api.mu.Unlock()
+
+	if api.subs.deposited == nil {
+		api.subs.deposited = make(map[int]subscriber[EventDdcCustomersDeposited])
+	}
+
+	var idx int
+	for i := 0; i <= math.MaxInt; i++ {
+		if _, ok := api.subs.deposited[i]; !ok {
+			idx = i
+			break
+		}
+	}
+
+	ch := make(chan EventDdcCustomersDeposited)
+	done := make(chan struct{})
+	api.subs.deposited[idx] = subscriber[EventDdcCustomersDeposited]{ch, done}
+
+	return &NewEventSubscription[EventDdcCustomersDeposited]{
+		ch:   ch,
+		done: done,
+		onDone: func() {
+			api.mu.Lock()
+			delete(api.subs.deposited, idx)
+			api.mu.Unlock()
+		},
+	}, nil
+}
+
+func (api *ddcCustomersApi) SubscribeNewInitialDepositUnlock() (*NewEventSubscription[EventDdcCustomersInitialDepositUnlock], error) {
+	api.mu.Lock()
+	defer api.mu.Unlock()
+
+	if api.subs.initialDepositUnlock == nil {
+		api.subs.initialDepositUnlock = make(map[int]subscriber[EventDdcCustomersInitialDepositUnlock])
+	}
+
+	var idx int
+	for i := 0; i <= math.MaxInt; i++ {
+		if _, ok := api.subs.initialDepositUnlock[i]; !ok {
+			idx = i
+			break
+		}
+	}
+
+	ch := make(chan EventDdcCustomersInitialDepositUnlock)
+	done := make(chan struct{})
+	api.subs.initialDepositUnlock[idx] = subscriber[EventDdcCustomersInitialDepositUnlock]{ch, done}
+
+	return &NewEventSubscription[EventDdcCustomersInitialDepositUnlock]{
+		ch:   ch,
+		done: done,
+		onDone: func() {
+			api.mu.Lock()
+			delete(api.subs.initialDepositUnlock, idx)
+			api.mu.Unlock()
+		},
+	}, nil
+}
+
+func (api *ddcCustomersApi) SubscribeNewWithdrawn() (*NewEventSubscription[EventDdcCustomersWithdrawn], error) {
+	api.mu.Lock()
+	defer api.mu.Unlock()
+
+	if api.subs.withdrawn == nil {
+		api.subs.withdrawn = make(map[int]subscriber[EventDdcCustomersWithdrawn])
+	}
+
+	var idx int
+	for i := 0; i <= math.MaxInt; i++ {
+		if _, ok := api.subs.withdrawn[i]; !ok {
+			idx = i
+			break
+		}
+	}
+
+	ch := make(chan EventDdcCustomersWithdrawn)
+	done := make(chan struct{})
+	api.subs.withdrawn[idx] = subscriber[EventDdcCustomersWithdrawn]{ch, done}
+
+	return &NewEventSubscription[EventDdcCustomersWithdrawn]{
+		ch:   ch,
+		done: done,
+		onDone: func() {
+			api.mu.Lock()
+			delete(api.subs.withdrawn, idx)
+			api.mu.Unlock()
+		},
+	}, nil
+}
+
+func (api *ddcCustomersApi) SubscribeNewCharged() (*NewEventSubscription[EventDdcCustomersCharged], error) {
+	api.mu.Lock()
+	defer api.mu.Unlock()
+
+	if api.subs.charged == nil {
+		api.subs.charged = make(map[int]subscriber[EventDdcCustomersCharged])
+	}
+
+	var idx int
+	for i := 0; i <= math.MaxInt; i++ {
+		if _, ok := api.subs.charged[i]; !ok {
+			idx = i
+			break
+		}
+	}
+
+	ch := make(chan EventDdcCustomersCharged)
+	done := make(chan struct{})
+	api.subs.charged[idx] = subscriber[EventDdcCustomersCharged]{ch, done}
+
+	return &NewEventSubscription[EventDdcCustomersCharged]{
+		ch:   ch,
+		done: done,
+		onDone: func() {
+			api.mu.Lock()
+			delete(api.subs.charged, idx)
+			api.mu.Unlock()
+		},
+	}, nil
+}
+
+func (api *ddcCustomersApi) SubscribeNewBucketCreated() (*NewEventSubscription[EventDdcCustomersBucketCreated], error) {
+	api.mu.Lock()
+	defer api.mu.Unlock()
+
+	if api.subs.bucketCreated == nil {
+		api.subs.bucketCreated = make(map[int]subscriber[EventDdcCustomersBucketCreated])
+	}
+
+	var idx int
+	for i := 0; i <= math.MaxInt; i++ {
+		if _, ok := api.subs.bucketCreated[i]; !ok {
+			idx = i
+			break
+		}
+	}
+
+	ch := make(chan EventDdcCustomersBucketCreated)
+	done := make(chan struct{})
+	api.subs.bucketCreated[idx] = subscriber[EventDdcCustomersBucketCreated]{ch, done}
+
+	return &NewEventSubscription[EventDdcCustomersBucketCreated]{
+		ch:   ch,
+		done: done,
+		onDone: func() {
+			api.mu.Lock()
+			delete(api.subs.bucketCreated, idx)
+			api.mu.Unlock()
+		},
+	}, nil
+}
+
+func (api *ddcCustomersApi) SubscribeNewBucketUpdated() (*NewEventSubscription[EventDdcCustomersBucketUpdated], error) {
+	api.mu.Lock()
+	defer api.mu.Unlock()
+
+	if api.subs.bucketUpdated == nil {
+		api.subs.bucketUpdated = make(map[int]subscriber[EventDdcCustomersBucketUpdated])
+	}
+
+	var idx int
+	for i := 0; i <= math.MaxInt; i++ {
+		if _, ok := api.subs.bucketUpdated[i]; !ok {
+			idx = i
+			break
+		}
+	}
+
+	ch := make(chan EventDdcCustomersBucketUpdated)
+	done := make(chan struct{})
+	api.subs.bucketUpdated[idx] = subscriber[EventDdcCustomersBucketUpdated]{ch, done}
+
+	return &NewEventSubscription[EventDdcCustomersBucketUpdated]{
+		ch:   ch,
+		done: done,
+		onDone: func() {
+			api.mu.Lock()
+			delete(api.subs.bucketUpdated, idx)
+			api.mu.Unlock()
+		},
+	}, nil
 }
