ccc8ddcf4109d9d59f485d942988775cb323357b
Alisher A. Khassanov <a.khssnv@gmail.com>
2024-01-03 18:59:59 +0600

    Add `pallet-ddc-payouts` event types

--- blockchain/pallets/events.go
+++ blockchain/pallets/events.go
@@ -21,6 +21,21 @@ type Events struct {
 	DdcCustomers_Charged              []EventDdcCustomersCharged              //nolint:stylecheck,golint
 	DdcCustomers_BucketCreated        []EventDdcCustomersBucketCreated        //nolint:stylecheck,golint
 	DdcCustomers_BucketUpdated        []EventDdcCustomersBucketUpdated        //nolint:stylecheck,golint
+
+	DdcPayouts_BillingReportInitialized    []EventDdcPayoutsBillingReportInitialized    //nolint:stylecheck,golint
+	DdcPayouts_ChargingStarted             []EventDdcPayoutsChargingStarted             //nolint:stylecheck,golint
+	DdcPayouts_Charged                     []EventDdcPayoutsCharged                     //nolint:stylecheck,golint
+	DdcPayouts_ChargeFailed                []EventDdcPayoutsChargeFailed                //nolint:stylecheck,golint
+	DdcPayouts_Indebted                    []EventDdcPayoutsIndebted                    //nolint:stylecheck,golint
+	DdcPayouts_ChargingFinished            []EventDdcPayoutsChargingFinished            //nolint:stylecheck,golint
+	DdcPayouts_TreasuryFeesCollected       []EventDdcPayoutsTreasuryFeesCollected       //nolint:stylecheck,golint
+	DdcPayouts_ClusterReserveFeesCollected []EventDdcPayoutsClusterReserveFeesCollected //nolint:stylecheck,golint
+	DdcPayouts_ValidatorFeesCollected      []EventDdcPayoutsValidatorFeesCollected      //nolint:stylecheck,golint
+	DdcPayouts_RewardingStarted            []EventDdcPayoutsRewardingStarted            //nolint:stylecheck,golint
+	DdcPayouts_Rewarded                    []EventDdcPayoutsRewarded                    //nolint:stylecheck,golint
+	DdcPayouts_RewardingFinished           []EventDdcPayoutsRewardingFinished           //nolint:stylecheck,golint
+	DdcPayouts_BillingReportFinalized      []EventDdcPayoutsBillingReportFinalized      //nolint:stylecheck,golint
+	DdcPayouts_AuthorisedCaller            []EventDdcPayoutsAuthorisedCaller            //nolint:stylecheck,golint
 }
 
 type NewEventSubscription[T any] struct {
