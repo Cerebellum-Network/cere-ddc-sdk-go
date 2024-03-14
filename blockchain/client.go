d77d31a81255b6fd1764cdfd931803311ed2c1c2
Alisher A. Khassanov <a.khssnv@gmail.com>
2024-03-14 14:15:43 +0500

    Skip undecodable events

--- blockchain/client.go
+++ blockchain/client.go
@@ -92,6 +92,7 @@ func (c *Client) StartEventsListening() (func(), <-chan error, error) {
 					err = types.EventRecordsRaw(change.StorageData).DecodeEventRecords(meta, events)
 					if err != nil {
 						c.errsListening <- fmt.Errorf("events decoder: %w", err)
+						continue
 					}
 
 					for _, callback := range c.eventsListeners {
